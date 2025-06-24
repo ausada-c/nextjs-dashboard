a43560f677d50786f79287dd61fad83d3267f456
ausada.chu <ausada.chu@gmail.com>
2025-06-25 00:06:00 +0700

    [Add] - edit page with log id

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
+    const id = params.id;
+    return (
+        <h1>
+            Hello, this is the edit page for invoice {id}
+        </h1>
+    );
+}
